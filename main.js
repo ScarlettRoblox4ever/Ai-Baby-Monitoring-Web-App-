ab1fe86c002078c56aee3e8c5af9e03493df572c
ScarlettRoblox4ever <99742376+ScarlettRoblox4ever@users.noreply.github.com>
2022-10-07 15:07:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var r;
+var g;
+var b;
+
+
+
+function setup() { 
+  createCanvas(400, 400);
+  rectMode(CENTER);
+} 
+
+function draw() { 
+  
+  background(220);
+  
+  r = random(255);
+  g = random(100,200); 
+  b = random(100); 
+}
